7a1624c86e903a0cb6ad8bc4cb0dcff5ff2d2571
eloisasacaca <eloisa.sacaca02@gmail.com>
2025-04-14 16:42:49 -0300

    Verificação de intervalo

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector("#inicial").value)
     const max = Math.floor(document.querySelector("#final").value)
     const resultTela = document.querySelector(".result")
+    
+    
+    
+    if (min >= max){
+        resultTela.style.color = "red"
+        resultTela.innerHTML = "Não é possível sortear um número, verifique se o intervalo está correto"
+    } else{
+     resultTela.style.color = "#343674"
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
     resultTela.innerHTML = "O número sorteado é " + result
-
-
+    }
 
 }
 
